c36639a29e5b2acbfc02e6ddee0ff900c9c49eca
Alejandro Liu <alejandro_liu@hotmail.com>
2015-11-27 00:58:40 +0100

    Tested on Linux

--- emacs/etc/custom-cua.el
+++ emacs/etc/custom-cua.el
@@ -19,13 +19,22 @@
 (global-set-key (kbd "M-s") 'write-file) ; C-x C-w
 (global-set-key (kbd "C-f") 'isearch-forward) ; was C-s
 (global-set-key (kbd "S-C-f") 'isearch-backward) ; was C-r
-(global-set-key (kbd "C-q") 'save-buffers-kill-terminal) ; was C-x C-c
+(global-set-key (kbd "C-q") 'save-buffers-kill-emacs) ; was C-x C-c
 (global-set-key (kbd "M-<f4>") 'save-buffers-kill-terminal) ; was C-x C-c
-(global-set-key (kbd "M-<delete>") 'kill-line) ; was Ctrl-k
+(global-set-key (kbd "C-<delete>") 'kill-line) ; was C-k
+(global-set-key (kbd "S-C-<delete>") 'kill-word) ; was M-d
+
+; should be kill beginning of line
+(defun backward-kill-line (arg)
+  "Kill ARG lines backward."
+  (interactive "p")
+  (kill-line (- 1 arg)))
+(global-set-key (kbd "C-<backspace>") 'backward-kill-line)
+(global-set-key (kbd "S-C-<backspace>") 'backward-kill-word)
 (global-set-key (kbd "M-o") 'open-line) ; was Ctrl-o
+(global-set-key (kbd "C-a") 'mark-whole-buffer) ; C-x h
 
 (global-set-key (kbd "C-<insert>") 'quoted-insert) ; was Ctrl-q
-(global-set-key (kbd "C-.") 'keyboard-quit) ; was Ctrl-g
 (global-set-key (kbd "C--") 'suspend-frame) ; was Ctrl-z
 (global-set-key (kbd "C-<") 'kmacro-start-macro) ; C-x (
 (global-set-key (kbd "C->") 'kmacro-end-macro) ; C-x )
@@ -33,40 +42,34 @@
 (global-set-key (kbd "S-C-r") 'kmacro-start-macro) ; C-x (
 (global-set-key (kbd "S-C-e") 'kmacro-end-or-call-macro) ; C-x ) or C-x e
 
-(global-set-key (kbd "M-g") 'goto-line) ; 
+(global-set-key (kbd "M-g") 'goto-line)
 
 (global-set-key (kbd "C-/") 'cua-exchange-point-and-mark) ; C-x C-x
 
 ; Semi Evil mode
 (global-set-key (kbd "C-h") 'backward-char)
-(global-set-key (kbd "M-C-h") 'beginning-of-line)
-(global-set-key (kbd "C-j") 'previous-line)
-(global-set-key (kbd "M-C-j") 'beginning-of-buffer)
-(global-set-key (kbd "C-k") 'next-line)
-(global-set-key (kbd "M-C-k") 'end-of-buffer)
+(global-set-key (kbd "C-j") 'next-line)
+(global-set-key (kbd "C-k") 'previous-line)
 (global-set-key (kbd "C-l") 'forward-char)
-(global-set-key (kbd "M-C-l") 'end-of-line)
 
 (global-set-key (kbd "C-<tab>") 'other-window) ; was C-x o
+; This only seems to work on Windows
 (global-set-key (kbd "S-C-<tab>") 'other-window); was C-x o
+(global-set-key (kbd "S-C-<insert>") 'insert-file) ; C-x i
 (global-set-key (kbd "C-w") 'kill-buffer) ; was C-x k
-(global-set-key (kbd "C-<f4>") 'delete-window) ; C-x 0
 (global-set-key (kbd "C-`") 'list-buffers) ; C-x C-b
 (global-set-key (kbd "S-C-g") 'switch-to-buffer) ; C-x b
-(global-set-key (kbd "C-<f1>") 'delete-other-windows) ; C-x 1
-(global-set-key (kbd "C-<f2>") 'split-window-below) ; C-x 2
-(global-set-key (kbd "C-<f3>") 'split-window-right) ; C-x 3
-(global-set-key (kbd "M-<insert>") 'insert-file) ; C-x i
+
+(global-set-key (kbd "C-<kp-0>") 'delete-window) ; C-x 0
+(global-set-key (kbd "C-<kp-1>") 'delete-other-windows) ; C-x 1
+(global-set-key (kbd "C-<kp-2>") 'split-window-below) ; C-x 2
+(global-set-key (kbd "C-<kp-3>") 'split-window-right) ; C-x 3
+
 
 ;  Don't know how to implement these...
-; should be kill beginning of line
-(global-set-key (kbd "M-<backspace>") 'kill-whole-line)
 ; should be new file
 (global-set-key (kbd "C-n") 'find-file) ; was C-x C-f
 ;global-set-key (kbd "C-y") 'repeat) ; redo
-; ctrl-a select all
-; repeat last search: f3 (emacs: defining kbd macro)
-; repeat last search back: shift f3 (emacs: defining kbd macro)
 
 ; standard Emacs bindings
 ; pgup : scroll-down-command
@@ -81,6 +84,8 @@
 ; C-right : right-word
 ; C-backspace : backward-kill-word
 ; C-delete : kill-word
+; f3 : start-macro
+; f4 : stop-macro-or-execute-macro
 
 ; CUA bindings
 ; ctrl+x : cut (if mark set, otherwise, prefix ctrl-x)
