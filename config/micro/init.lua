14cbfe033ab21472ff823aafb837dda4f3801676
Alejandro Liu <alejandro_liu@hotmail.com>
2022-06-07 19:03:27 +0200

    micro editor updates

---
+++ config/micro/init.lua
@@ -0,0 +1,20 @@
+local config = import("micro/config")
+local micro = import("micro")
+--~ local shell = import("micro/shell")
+
+function MouseToggle()
+  cmouse = config.GetGlobalOption("mouse")
+  if cmouse then
+    config.SetGlobalOption("mouse","false")
+    micro.InfoBar():Message("Mouse control is now ENABLED")
+  else
+    config.SetGlobalOption("mouse","true")
+    micro.InfoBar():Message("Mouse control is now DISABLED")
+  end
+
+end
+
+function init()
+  config.TryBindKey("Alt-m", "lua:initlua.MouseToggle", true)
+end
+
