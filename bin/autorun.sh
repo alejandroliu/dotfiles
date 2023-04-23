c0e5d0a4ffb933d66e1e447c839b3232335f431f
Alejandro Liu <alejandro_liu@hotmail.com>
2023-04-23 08:59:30 +0200

    migrating to local-startup

--- bin/autorun.sh
+++ bin/autorun.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 #~ export PATH=$PATH:$HOME/.bin:$HOME/.gitbin
+sleep 3 # Delay so that sytem tray is ready
 
-(xdpyinfo | grep -q XVNC-EXTENSION) && IS_VIRTUAL=true
+(xdpyinfo | grep -q XVNC-EXTENSION) && IS_VIRTUAL=true || IS_VIRTUAL=false
 
-if ! $IS_VIRTUAL ; then
-  :
-  # type safeeyes && ( sleep 10 ; safeeyes ) &
-  # type redshift-gtk && redshift-gtk & # Red shift display
-fi
+#~ if ! $IS_VIRTUAL ; then
+  #~ if (type safeeyes && ! (ps x | grep python | grep safeeyes)); then
+    #~ # Safe eyes is not running yet
+    #~ safeeyes &
+  #~ fi
+  #~ # type redshift-gtk && redshift-gtk & # Red shift display
+#~ fi
 
 #~ if [ -n "$(pidof xconsole)" ] ; then
   #~ type xconsole-helper && xconsole-helper &
 #~ fi
 
-type pidgin && pidgin & # local chat client
+#~ type pidgin && pidgin & # local chat client
 # type parcellite && parcellite & # clipboard manager
 
